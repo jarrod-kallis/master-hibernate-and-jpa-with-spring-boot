779314fd5ac892d6fbf370a1bff8397d861f951d
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-07-16 18:05:00 +0200

    Spring Boot in 10 Steps

---
+++ springboot-in-10-steps/README.md
@@ -0,0 +1,8 @@
+# Spring Boot in 10 Steps
+
+### Section 2: Introduction to Spring Boot in 10 Steps
+
+All code can be found here: https://github.com/in28minutes/jpa-with-hibernate/tree/master/00-framework-tool-introductions/02.Spring-Boot-Introduction-In-10-Steps
+
+![Initialise Spring App](README_images/01-Spring-Initializr.png)
+
---
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+	}
+
+}
